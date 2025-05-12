e094b90b74af26481243e3b18c6abb910394cd0f
acme-inter <web-support@acme-inter.com>
2025-05-12 04:23:28 -0400

    update code

--- service/routes.py
+++ service/routes.py
@@ -1,8 +1,12 @@
+"""
+Controller for routes
+"""
 from flask import jsonify, url_for, abort
 from service import app
 from service.common import status
 
 COUNTER = {}
+
 
 ############################################################
 # Health Endpoint
@@ -18,7 +22,7 @@ def health():
 ############################################################
 @app.route("/")
 def index():
-    """Returns information about the service"""
+    """Returns information abut the service"""
     app.logger.info("Request for Base URL")
     return jsonify(
         status=status.HTTP_200_OK,
@@ -36,9 +40,7 @@ def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
 
-    counters = [
-        dict(name=count[0], counter=count[1]) for count in COUNTER.items()
-    ]
+    counters = [dict(name=count[0], counter=count[1]) for count in COUNTER.items()]
 
     return jsonify(counters)
 
@@ -52,10 +54,7 @@ def create_counters(name):
     app.logger.info("Request to Create counter: %s...", name)
 
     if name in COUNTER:
-        return abort(
-            status.HTTP_409_CONFLICT, 
-            f"Counter {name} already exists"
-        )
+        return abort(status.HTTP_409_CONFLICT, f"Counter {name} already exists")
 
     COUNTER[name] = 0
 
@@ -76,10 +75,7 @@ def read_counters(name):
     app.logger.info("Request to Read counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(
-            status.HTTP_404_NOT_FOUND, 
-            f"Counter {name} does not exist"
-        )
+        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
 
     counter = COUNTER[name]
     return jsonify(name=name, counter=counter)
@@ -94,10 +90,7 @@ def update_counters(name):
     app.logger.info("Request to Update counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(
-            status.HTTP_404_NOT_FOUND, 
-            f"Counter {name} does not exist"
-        )
+        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
 
     COUNTER[name] += 1
 
